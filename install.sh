8b9228fc85322b64a9c51fc3fb072b2f115c25a8
N Ξ Θ <paul.gardien@epitech.eu>
2020-01-23 13:39:00 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,11 +6,21 @@ install(){
         sudo chmod +x mms && sudo cp mms /usr/bin/
         if [[ -f "/usr/bin/mms" ]]; then
             printf "Status..........: Installed\n"
+            printf "Path..........: /usr/bin/mms\n"
         else
             printf "Status..........: Not installed\n"
         fi
     else
         printf "Status..........: Installed\n"
+        printf "Path..........: /usr/bin/mms\n"
+        printf "Do you want to update [y/n]\n"
+        printf "Choice..........: "
+        read update
+        if [[ $update == "y" ]]; then
+            sudo rm /usr/bin/mms
+            sudo chmod +x mms && sudo cp mms /usr/bin/
+            install
+        fi
     fi
     }
 
